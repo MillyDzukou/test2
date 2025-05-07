a561ea51b8b02bd3ce5d0572147779ab5708c652
MillyDzukou <dzukoumilly2024>
2025-05-06 22:10:51 -0600

    An example of a for and a while loop

---
+++ hello.py
@@ -0,0 +1,13 @@
+"""
+This is a simple program to 
+print 'Hello World'
+"""
+
+for i in range(10):
+    print("Hello world for the {} th time".format(i))
+
+a=0
+while(a<10):
+    print("This is the {} th term".format(a))
+    a = a+1
+print("End of the while loop")
